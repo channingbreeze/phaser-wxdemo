9c28a5740de7f82cf55197e76684e5137aabcb2e
竹逊 <channingbreeze@163.com>
2018-01-27 20:53:02 +0800

    basic load an image

--- js/basic/BasicLoadAnImageState.js
+++ js/basic/BasicLoadAnImageState.js
@@ -1,20 +1,27 @@
 import Phaser from '../libs/phaser-wx.js';
 
-export default class BootState extends Phaser.State {
+export default class BasicLoadAnImageState extends Phaser.State {
 	
 	constructor(game) {
 		super();
 		this.game = game;
 	}
 
-	create() {
-		// this is very important
-		this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+	preload() {
+		// 加载一个图片，之后可通过 einstein 来引用
+    this.game.load.image('einstein', 'assets/basic/ra_einstein.jpg');
+	}
 
-		// invalid sound lock
-		this.game.sound.touchLocked = false;
-		
-		this.game.state.start('animation');
+	create() {
+		// 在(0,0)位置显示该图片
+    this.game.add.sprite(0, 0, 'einstein');
+
+    // tap屏幕的时候，回到menu
+    this.game.input.onTap.add(this.onTap, this);
+	}
+
+	onTap() {
+		this.game.state.start('menu');
 	}
 
 }
--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ import BootState from 'states/BootState.js'
 import AnimationState from 'states/AnimationState.js'
 import MenuState from 'states/MenuState.js'
 
+import BasicLoadAnImageState from 'basic/BasicLoadAnImageState.js'
+
 import PlanePreloadState from 'plane/states/PlanePreloadState.js'
 import PlaneGameState from 'plane/states/PlaneGameState.js'
 
@@ -16,6 +18,8 @@ var game = new Phaser.Game({
 game.state.add('boot', new BootState(game));
 game.state.add('animation', new AnimationState(game));
 game.state.add('menu', new MenuState(game));
+
+game.state.add('basicLoadAnImage', new BasicLoadAnImageState(game));
 
 game.state.add('planePreload', new PlanePreloadState(game));
 game.state.add('planeGame', new PlaneGameState(game));
